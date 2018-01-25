8f8f01e51e92af9ad0e08adbc6d6ef8211430998
djairjr <djair.jr@gmail.com>
2018-01-25 09:57:20 -0200

    Update main.py
    Change SPI call, in order to work with Loboris Micropython with my ESP32 DEV Kit.

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 from machine import SPI, Pin
 
 # WiPy (on Exp board, SD and User-LED jumper have to be removed!)
-SPI    = machine.SPI(0)   # GP14 (CLK) + GP16 (MOSI->DIN), User-LED jumper removed!
-RST    = machine.Pin('GP24')
-CE     = machine.Pin('GP12')
-DC     = machine.Pin('GP22')
-LIGHT  = machine.Pin('GP23')
+SPI    = machine.SPI(1, baudrate=328125, bits=8, polarity=0, phase=1, sck=18, mosi=23, miso=19)   # Changed this line in order to work with ESP32 DEV Kit with Loboris Micropython
+RST    = machine.Pin(4)
+CE     = machine.Pin(5)
+DC     = machine.Pin(19)
+LIGHT  = machine.Pin(2)
 # PWR    = directly from 3V3 pin of the WiPy
 
 import upcd8544
